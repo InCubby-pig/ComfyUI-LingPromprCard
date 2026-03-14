a9b76ed78f392e9173be22eb16498cb321bce03b
“inCubby-pig” <1511843297@qq.com>
2026-03-14 23:22:10 +0800

    * 拆分风格

---
+++ promptcard/data/danbooru/danbooru_selector_danbooru_visual_image_composition_and_style_focus_tags.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""自动生成文件：由 tools/extract_danbooru_tag_groups.py 生成，请勿手改。"""
+
+SELECTOR_NODE_KEY = 'danbooru_selector_danbooru_visual_image_composition_and_style_focus_tags'
+SELECTOR_CLASS_NAME = 'LingPromptCardDanbooruSelectorDanbooruVisualImageCompositionAndStyleFocusTags'
+SELECTOR_DISPLAY_NAME = 'Danbooru-视觉-Image Composition And Style-Focus Tags'
+SELECTOR_BUCKET = 'danbooru_visual'
+SELECTOR_CATEGORY = 'Image composition and style'
+SELECTOR_SOURCE = 'tag_group:focus_tags'
+
+SELECTOR_DATA = {'title': 'Danbooru-视觉-Image Composition And Style-Focus Tags', 'display_title': 'Danbooru-视觉-Image Composition And Style-Focus Tags', 'sections': ['Visual characteristics'], 'categories': [{'label': 'Image Composition And Style / Focus Tags', 'display_label': '图像构成和风格 / Focus Tags | Image Composition And Style / Focus Tags', 'input_key': 'Image Composition And Style / Focus Tags', 'items': [{'id': 'danbooru_selector_danbooru_visual_image_composition_and_style_focus_tags_b21e2d7deaa1', 'title': 'clothes_focus', 'display_title': '衣服焦点 | clothes_focus', 'group_title': 'Objects', 'group_path': 'Visual characteristics / Image composition and style / tag_group:focus_tags / Objects', 'source_page': 'tag_group:focus_tags', 'prompt_pos': 'clothes_focus, ', 'prompt_neg': ''}, {'id': 'danbooru_selector_danbooru_visual_image_composition_and_style_focus_tags_014d2d696f5e', 'title': 'footwear_focus', 'display_title': '鞋类焦点 | footwear_focus', 'group_title': 'Objects', 'group_path': 'Visual characteristics / Image composition and style / tag_group:focus_tags / Objects', 'source_page': 'tag_group:focus_tags', 'prompt_pos': 'footwear_focus, ', 'prompt_neg': ''}, {'id': 'danbooru_selector_danbooru_visual_image_composition_and_style_focus_tags_562b2c2b10cd', 'title': 'leg_focus', 'display_title': '腿部焦点 | leg_focus', 'group_title': 'Body parts', 'group_path': 'Visual characteristics / Image composition and style / tag_group:focus_tags / Body parts', 'source_page': 'tag_group:focus_tags', 'prompt_pos': 'leg_focus, ', 'prompt_neg': ''}]}]}
---
+++ promptcard/data/danbooru/danbooru_selector_danbooru_visual_image_composition_and_style_patterns.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""自动生成文件：由 tools/extract_danbooru_tag_groups.py 生成，请勿手改。"""
+
+SELECTOR_NODE_KEY = 'danbooru_selector_danbooru_visual_image_composition_and_style_patterns'
+SELECTOR_CLASS_NAME = 'LingPromptCardDanbooruSelectorDanbooruVisualImageCompositionAndStylePatterns'
+SELECTOR_DISPLAY_NAME = 'Danbooru-视觉-Image Composition And Style-Patterns'
+SELECTOR_BUCKET = 'danbooru_visual'
+SELECTOR_CATEGORY = 'Image composition and style'
+SELECTOR_SOURCE = 'tag_group:patterns'
+
+SELECTOR_DATA = {'title': 'Danbooru-视觉-Image Composition And Style-Patterns', 'display_title': 'Danbooru-视觉-Image Composition And Style-Patterns', 'sections': ['Visual characteristics'], 'categories': [{'label': 'Image Composition And Style / Patterns', 'display_label': '图像构成和风格 / 图案 | Image Composition And Style / Patterns', 'input_key': 'Image Composition And Style / Patterns', 'items': [{'id': 'danbooru_selector_danbooru_visual_image_composition_and_style_patterns_094c10ef4c6a', 'title': 'shou_(symbol)', 'display_title': '受（符号） | shou_(symbol)', 'group_title': 'Chinese Patterns', 'group_path': 'Visual characteristics / Image composition and style / tag_group:patterns / Chinese Patterns', 'source_page': 'tag_group:patterns', 'prompt_pos': 'shou_(symbol), ', 'prompt_neg': ''}]}]}
